e8c542f4c2035cada4eb1e52db94b3821c769841
honeybunzzz77 <101166457+honeybunzzz77@users.noreply.github.com>
2022-03-08 01:14:03 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite'
+import postcss from './postcss.config.js'
+import react from '@vitejs/plugin-react'
+import NodeGlobalsPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
+  css: {
+    postcss,
+  },
+  plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        })
+      ],
+    },
+  },
+  commonjsOptions: {
+    transformMixedEsModules: true,
+  },
+  resolve: {
+    mainFields: ['browser', 'module', 'main'],
+    alias: [
+      {
+        find: /^~.+/,
+        replacement: (val) => {
+          return val.replace(/^~/, "");
+        },
+      },
+    ],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    }
+  } 
+})
