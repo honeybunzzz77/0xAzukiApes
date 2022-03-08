483182c7a9766a6deec080e01138a0857d34cb31
honeybunzzz77 <101166457+honeybunzzz77@users.noreply.github.com>
2022-03-08 03:00:52 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import NodeGlobalsPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   define: {
     'process.env': process.env
